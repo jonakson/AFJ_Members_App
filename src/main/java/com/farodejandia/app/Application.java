fede915efaca8f89589c86c877c0a8359fdbac93
jcalzado <jonathancalzado1988@gmail.com>
2021-12-30 18:49:54 +0000

    First frontend version ready

--- src/main/java/com/farodejandia/app/Application.java
+++ src/main/java/com/farodejandia/app/Application.java
@@ -1,24 +1,7 @@
 package com.farodejandia.app;
 
-import com.farodejandia.app.activity.Activity;
-import com.farodejandia.app.activity.ActivityRepository;
-import com.farodejandia.app.member.Member;
-import com.farodejandia.app.member.MemberRepository;
-import com.farodejandia.app.membership.Membership;
-import com.farodejandia.app.membership.MembershipRepository;
-import com.farodejandia.app.participant.Participant;
-import com.farodejandia.app.participant.ParticipantRepository;
-import com.farodejandia.app.payment.Payment;
-import com.farodejandia.app.payment.PaymentRepository;
-import com.github.javafaker.Faker;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Application {
@@ -27,108 +10,5 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(
-            MemberRepository memberRepository,
-            ActivityRepository activityRepository,
-            MembershipRepository membershipRepository,
-            PaymentRepository paymentRepository,
-            ParticipantRepository participantRepository) {
-        return args -> {
-
-            Member jonatan = new Member(
-                    "78535249R",
-                    "jonatan@farodejandia.com",
-                    "Jonatan",
-                    "Calzado Diaz",
-                    "666777888",
-                    LocalDateTime.now().minusYears(33L),
-                    LocalDateTime.now().minusYears(6L)
-            );
-
-            Activity travel = new Activity(
-                    "Travel to Lanzarote",
-                    "Travel to Lanzarote to visit Timanfaya",
-                    LocalDateTime.of(2022, 4, 28, 6, 30,0),
-                    35,
-                    "Port of Corralejo",
-                    77.50
-            );
-
-            Membership newMemberMembership = new Membership(
-                    "New Member Membership",
-                    "New members membership, paid once.",
-                    0,
-                    5.0
-            );
-            Membership annualMembership = new Membership(
-                    "Annual Membership",
-                    "Membership paid annually.",
-                    265,
-                    30.0
-            );
-
-            Payment jonatanNew = new Payment(
-                    jonatan,
-                    newMemberMembership,
-                    LocalDateTime.now());
-            Payment jonatanAnnual2022 = new Payment(
-                    jonatan,
-                    annualMembership,
-                    LocalDateTime.now());
-
-            Participant jonatanTravel = new Participant(
-                    jonatan,
-                    travel,
-                    LocalDateTime.now()
-            );
-
-            memberRepository.save(jonatan);
-            activityRepository.save(travel);
-            membershipRepository.save(newMemberMembership);
-            membershipRepository.save(annualMembership);
-            paymentRepository.save(jonatanNew);
-            paymentRepository.save(jonatanAnnual2022);
-            participantRepository.save(jonatanTravel);
-
-        };
-    }
-
-    private void generateRandomMembers(MemberRepository memberRepository) {
-        Faker faker = new Faker();
-        for (int i=0; i<25; i++) {
-            String idDocument = validIdGenerator(faker.number().randomNumber(8, true));
-            String name = faker.name().firstName();
-            String lastName = faker.name().lastName();
-            String email = String.format("%s.%s@farodejandia.com", name.toLowerCase(), lastName.toLowerCase());
-            String phone = faker.phoneNumber().cellPhone();
-            LocalDateTime dob = faker
-                    .date()
-                    .birthday()
-                    .toInstant()
-                    .atZone(ZoneId.systemDefault())
-                    .toLocalDateTime();
-            LocalDateTime entryDate = faker.
-                    date().
-                    past(1, TimeUnit.DAYS)
-                    .toInstant()
-                    .atZone(ZoneId.systemDefault())
-                    .toLocalDateTime();
-            Member member = new Member(
-                    idDocument,
-                    email,
-                    name,
-                    lastName,
-                    phone,
-                    dob,
-                    entryDate
-            );
-            memberRepository.save(member);
-        }
-    }
-
-    String validIdGenerator (Long number) {
-        return number.toString() + ("TRWAGMYFPDXBNJZSQVHLCKE".charAt((int) (number%23)));
-    }
 
 }
--- src/main/java/com/farodejandia/app/member/MemberController.java
+++ src/main/java/com/farodejandia/app/member/MemberController.java
@@ -1,12 +1,15 @@
 package com.farodejandia.app.member;
 
+import com.github.javafaker.Faker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(path = "api/v1/members")
@@ -14,28 +17,42 @@ public class MemberController {
 
     @GetMapping
     public List<Member> getAllMembers() {
-        Member juanjo = new Member(
-                "78532288F",
-                "juanjo@farodejandia.com",
-                "Juan José",
-                "Viera Rodríguez",
-                "666111000",
-                LocalDateTime.now().minusYears(33L),
-                LocalDateTime.now().minusYears(6L)
-        );
-        juanjo.setId(1L);
-        Member jonatan = new Member(
-                "78535249R",
-                "jonatan@farodejandia.com",
-                "Jonatan",
-                "Calzado Diaz",
-                "666777888",
-                LocalDateTime.now().minusYears(33L).minusMonths(3L),
-                LocalDateTime.now().minusYears(6L)
-        );
-        jonatan.setId(2L);
 
-        return  Arrays.asList(juanjo, jonatan);
+        List<Member> generatedMembers = new ArrayList<>();
+        Faker faker = new Faker();
+        for (int i=0; i<50; i++) {
+            Long number = faker.number().randomNumber(8, true);
+            String idDocument = number.toString() + ("TRWAGMYFPDXBNJZSQVHLCKE".charAt((int) (number%23)));
+            String name = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@farodejandia.com", name.toLowerCase(), lastName.toLowerCase());
+            String phone = faker.phoneNumber().cellPhone();
+            LocalDateTime dob = faker
+                    .date()
+                    .birthday()
+                    .toInstant()
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDateTime();
+            LocalDateTime entryDate = faker.
+                    date().
+                    past(2500, TimeUnit.DAYS)
+                    .toInstant()
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDateTime();
+            Member member = new Member(
+                    idDocument,
+                    email,
+                    name,
+                    lastName,
+                    phone,
+                    dob,
+                    entryDate
+            );
+            member.setId((long) i);
+            generatedMembers.add(member);
+        }
+
+        return  generatedMembers;
     }
 
 }
